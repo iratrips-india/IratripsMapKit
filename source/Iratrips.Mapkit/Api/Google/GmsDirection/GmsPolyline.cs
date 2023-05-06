ca2918ed9c0536971236e1045652e03b5ed3f9ea
Pravin <pravin@m1pro.local>
2023-05-06 10:11:18 +0530

    Received positions from the caller

--- source/Iratrips.Mapkit/Api/Google/GmsDirection/GmsPolyline.cs
+++ source/Iratrips.Mapkit/Api/Google/GmsDirection/GmsPolyline.cs
@@ -11,7 +11,21 @@ namespace Iratrips.Mapkit.Api.Google
         /// <summary>
         /// Gets the points as string
         /// </summary>
-        public string Points { get; set; }
+
+        private string _points = null;
+        public string Points
+        {
+            get => _points;
+            set
+            {
+                _points = value;
+                _positions = null;
+            }
+        }
+
+
+        private IEnumerable<Position> _positions = null;
+
         /// <summary>
         /// Gets the converted positions
         /// </summary>
@@ -20,8 +34,22 @@ namespace Iratrips.Mapkit.Api.Google
         {
             get
             {
-                return GooglePoints.Decode(Points);
+                if (_positions == null)
+                    _positions = GooglePoints.Decode(Points);
+
+                return _positions;
             }
+        }
+
+        public GmsPolyline()
+        {
+
+        }
+
+        public GmsPolyline(string points, IEnumerable<Position> positions)
+        {
+            this.Points = points;
+            _positions = positions;
         }
     }
 }
